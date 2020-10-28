52fdcd996b355e4e6fb1b90efeeb38108315c1ba
favorskij <favorskij@mail.ru>
2020-10-28 12:09:24 +1100

    Initial commit

---
+++ src/com/functionexample/Main.java
@@ -0,0 +1,15 @@
+package com.functionexample;
+
+import java.util.function.Function;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        Function<Integer, String> convert = x-> String.valueOf(x) + " долларов";
+
+        System.out.println(convert.apply(5)); // 5 долларов
+
+
+    }
+}
